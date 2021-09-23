6e46545e0487bbc0695d1f5701cf548c2f873703
YuanL12 <yuanluo@uchicago.edu>
2021-09-23 12:52:12 -0500

    Updated main brach

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,9 +16,9 @@ sys.path.insert(0, os.path.abspath('.'))
 
 
 # -- Project information -----------------------------------------------------
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Personal Notes'
+copyright = '2021, Yuan'
+author = 'Yuan Luo'
 
 # The full version, including alpha/beta/rc tags
 release = '0.1'
@@ -137,5 +137,8 @@ todo_include_todos = True
 myst_enable_extensions = [
     "colon_fence",
     "deflist",
+    "dollarmath",
+    "amsmath",
 ]
+myst_dmath_double_inline = True 
 myst_heading_anchors = 3
